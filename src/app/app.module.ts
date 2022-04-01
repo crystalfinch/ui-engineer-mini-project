84c1a4c76a9b6ab50c166623e2f6db0df7d3b68e
Crystal Finch <crystal.storm@gmail.com>
2022-03-31 17:18:36 -0700

    Header component and styles. Configure angular to look for scss imports in styles folder.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { CharactersComponent } from './characters/characters.component';
 import { EpisodesComponent } from './episodes/episodes.component';
 import { IndividualComponent } from './individual/individual.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { IndividualComponent } from './individual/individual.component';
     CharactersComponent,
     EpisodesComponent,
     IndividualComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
